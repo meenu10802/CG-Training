d59eea9e2778795566b4a0b179e87cd7735014c6
meenu10802 <meenu10802@gmail.com>
2025-12-15 23:20:57 +0530

    Git Task 4

---
+++ CGTraining/Day2/PaymentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGTraining.Day2
+{
+    internal class PaymentService
+    {
+
+        public static bool Sample()
+            {
+                Console.WriteLine("Enter payment");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                return true;
+        }
+    }
+}
