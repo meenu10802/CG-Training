d5a3f1052a437b261343268a7b48e16d330332c1
meenu10802 <meenu10802>
2025-12-17 12:45:33 +0530

    Fixed login null check bug

---
+++ CGTraining/LoginService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGTraining
+{
+    public class LoginService
+    {
+        public bool Login(string username, string password)
+        {
+            if (username == null || password == null)
+                return false;
+            return true;
+        }
+    }
+}
