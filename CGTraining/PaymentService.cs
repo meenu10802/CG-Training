97729cb2e8f0124c427362b4973dbe799aa3f4a1
meenu10802 <meenu10802@gmail.com>
2025-12-17 11:53:23 +0530

    branchA adds null check

--- CGTraining/PaymentService.cs
+++ CGTraining/PaymentService.cs
@@ -2,8 +2,12 @@ public class PaymentService
 {
     public bool ProcessPayment(Payment payment)
     {
+        if (payment == null)
+            return false;
+
         return true;
     }
+
 }
 
 public class Payment
