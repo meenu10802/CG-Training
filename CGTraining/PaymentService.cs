53d1c40ad00f12d5f6dda9c6b638ed2048d878f7
meenu10802 <meenu10802@gmail.com>
2025-12-17 11:15:45 +0530

    Git Task 4

---
+++ CGTraining/PaymentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGTraining
+{
+    public class PaymentService
+    {
+        public bool ProcessPayment(Payment payment)
+        {
+            return true;
+        }
+    }
+
+    public class Payment
+    {
+        public double Amount { get; set; }
+    }
+}
